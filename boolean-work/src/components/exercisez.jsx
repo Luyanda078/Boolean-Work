984db2a5388f7bd0be6ca16c172c0ff0b5a7fd52
Luyanda <luyandashamase5@gmail.com>
2024-08-21 13:03:46 +0200

    Task almost done

--- boolean-work/src/components/exercisez.jsx
+++ boolean-work/src/components/exercisez.jsx
@@ -14,23 +14,38 @@ export default function Exercisez() {
   };
 
   return (
-    <div>
+    <div >
+      <div >
       <div style={{ position: 'relative' }}>
         <img src="https://th.bing.com/th/id/OIP.PLLWGq3IOvtphVdLG3zmGAHaEc?w=288&h=180&c=7&r=0&o=5&pid=1.7" alt="Exercise Image" style={{ width: '100vw', height: '50vh', objectFit: 'cover' }} />
         <h1 style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: '#fff', fontSize: '36px', fontWeight: 'bold' }}>This is your exercise page</h1>
       </div>
-      <footer style={{ backgroundColor: '#333', color: '#fff', padding: '20px', textAlign: 'center', height: '9vh' }}>
+      <footer style={{ backgroundColor: '#333', color: '#fff', padding: '5%', textAlign: 'center', height: '9vh' }}>
         <p> </p>
       </footer>
-      <h3>What is the biggest number?</h3>
-          <form onSubmit={handleSubmit}>
-            <label>Num 1</label>
-            <input type="number" value={num1} onChange={(e) => setNum1(e.target.value)} placeholder="Enter num1" /><br></br><br></br>
-            <label>Num 2</label>
-            <input type="number" value={num2} onChange={(e) => setNum2(e.target.value)} placeholder="Enter num2" /><br></br><br></br>
-            <button type="submit">Calculate</button>
-          </form>
-          <p>{result}</p>
+      <div style={{border: '1px solid #ccc',padding: '20px',borderRadius: '10px',
+        width: '25%',
+        marginLeft: '20px',
+        float: 'left',
+        borderBlockColor:'blueviolet',
+        background:'skyblue'
+
+      }}>
+        <h3 style={{ color: 'blue' }}>What is the biggest number?</h3>
+        <form onSubmit={handleSubmit}>
+          <label>Num 1</label>
+          <input type="number" value={num1} onChange={(e) => setNum1(e.target.value)} placeholder="Enter num1" /><br></br><br></br>
+          <label>Num 2</label>
+          <input type="number" value={num2} onChange={(e) => setNum2(e.target.value)} placeholder="Enter num2" /><br></br><br></br>
+          <button type="submit" textAlign="center">Calculate </button>
+        </form>
+        <div className='result' style={{border:"5px solid blue ", width:"102%", height:"25%", marginLeft:"-1%", marginTop:"9.2%", backgroundColor:"pink"}} >
+       <p>{result}</p>
+</div>
+      </div>
+
+      </div>
+     
     </div>
     
   );
--- boolean-work/src/components/footer.jsx
+++ boolean-work/src/components/footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div>
 
-      <p>This is a portal</p>
+      <p></p>
       
     </div>
   )
